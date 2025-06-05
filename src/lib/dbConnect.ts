a7cb8c4220ed7b17c3213b266960e4f4315a6703
Aman Sheikh <haarishsheikh04@gmail.com>
2025-06-05 14:37:02 +0530

    Complete Setup resend email with Next JS

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to databse");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+    console.log(db);
+
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log("DB Connected successfully");
+  } catch (error) {
+    console.log("Databse connection failed", error);
+
+    process.exit();
+  }
+}
+
+export default dbConnect;
