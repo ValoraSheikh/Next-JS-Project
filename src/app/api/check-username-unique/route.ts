20c35298c619958034bfe5296f62e925634f4c06
Aman Sheikh <haarishsheikh04@gmail.com>
2025-06-09 15:13:11 +0530

    Complete Message API with aggregation pipline

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,70 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User.model";
+import { usernameValidation } from "@/Schemas/signUpSchema";
+import { z } from "zod";
+
+const UsernameQuerySchema = z.object({
+  username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  try {
+    const { searchParams } = new URL(request.url);
+    const queryParam = {
+      username: searchParams.get("username"),
+    };
+
+    const result = UsernameQuerySchema.safeParse(queryParam);
+    console.log(result);
+
+    if (!result.success) {
+      const usernameErrors = result.error.format().username?._errors || [];
+      return Response.json(
+        {
+          success: false,
+          message:
+            usernameErrors?.length > 0
+              ? usernameErrors.join(", ")
+              : "Invalid query parameters",
+        },
+        { status: 400 }
+      );
+    }
+
+    const { username } = result.data;
+
+    const existingVerfiedUser = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingVerfiedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken",
+        },
+        { status: 400 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Username is avaiable",
+      },
+      { status: 400 }
+    );
+  } catch (error) {
+    console.log("Error checking username", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error checking username",
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,60 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User.model";
+import { User } from "next-auth";
+import mongoose from "mongoose";
+import second from "@/models/User.model";
+import { Message } from "@/app/User.model";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  const { username, content } = await request.json();
+
+  try {
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "usernot found",
+        },
+        { status: 404 }
+      );
+    }
+
+    if (!user?.isAcceptingMessages) {
+      return Response.json(
+        {
+          success: false,
+          message: "user in not accepting messages",
+        },
+        { status: 403 }
+      );
+    }
+
+    const newMessage = { content, createdAt: new Date() };
+    user.messages.push(newMessage as Message);
+
+    await user.save();
+
+    return Response.json(
+      {
+        success: true,
+        message: "mesasge sent successfully",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Getting errors in sending messages", error)
+    return Response.json(
+        {
+          success: false,
+          message: "Failed to sending messages",
+        },
+        { status: 500 }
+      );
+  }
+}
